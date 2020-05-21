69e70351693a61bd232dff7045d8feca08559364
ydroulis <lombardi.droulis@gmail.com>
2020-05-21 19:36:06 -0300

    componente Teamwork estilizado

--- src/components/Teamwork/Teamwork.jsx
+++ src/components/Teamwork/Teamwork.jsx
@@ -4,23 +4,27 @@ import imgCamila from './img/camila.png'
 import imgGuto from './img/guto.png'
 import imgDavid from './img/david.png'
 import imgBeatriz from './img/beatriz.png'
+import './Teamwork.css'
 
 export default Teamwork => {
-
-
     return (
         <article className="teamwork">
             <section className="ceo">
                 <img src={video} alt="placeholder-video" />
-                <h1>Palavra do CEO</h1>
-                <h2>Carlos Curioni</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis, tellus vitae malesuada elementum, ex justo blandit orci, nec lacinia risus purus vel mi. In hac habitasse platea dictumst. Nunc at felis in nibh pretium imperdiet vitae id augue.</p>
+                <div className="ceo-content">
+                    <h1>Palavra do CEO</h1>
+                    <h2>Carlos Curioni</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis, tellus vitae malesuada elementum, ex justo blandit orci, nec lacinia risus purus vel mi. In hac habitasse platea dictumst. Nunc at felis in nibh pretium imperdiet vitae id augue.</p>
+                </div>
             </section>
-            <section className="teamCards">
-                <div className="card"><img src={imgCamila} alt="Foto da Camila" /></div>
-                <div className="card"><img src={imgGuto} alt="foto do Guto" /></div>
-                <div className="card"><img src={imgDavid} alt="Foto do David" /></div>
-                <div className="card"><img src={imgBeatriz} alt="Foto da Beatriz" /></div>
+            <section className="team">
+                <h1>Conheça nosso time fora de série</h1>
+                <div className = "cards">
+                    <div className="card"><a href="#"><img src={imgCamila} alt="Foto da Camila" /></a></div>
+                    <div className="card"><a href="#"><img src={imgGuto} alt="foto do Guto" /></a></div>
+                    <div className="card"><a href="#"><img src={imgDavid} alt="Foto do David" /></a></div>
+                    <div className="card"><a href="#"><img src={imgBeatriz} alt="Foto da Beatriz" /></a></div>
+                </div>
             </section>
         </article>
     )
